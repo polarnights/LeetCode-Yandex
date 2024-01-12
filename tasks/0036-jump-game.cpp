add659c2ce80985836e04f564c72dd2169c6de9f
Nick Oliger <naoliger@edu.hse.ru>
2024-01-13 00:14:01 +0300

    13/01/24 | Add solution for task #36

---
+++ tasks/0036-jump-game.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool canJump(vector<int> &nums) {
+        int current_max = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > current_max) {
+                return false;
+            }
+            current_max = max(current_max, i + nums[i]);
+            if (current_max >= nums.size() - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // This idea is actually pretty cool :D
+};
