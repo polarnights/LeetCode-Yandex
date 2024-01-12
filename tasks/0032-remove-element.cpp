b38107bd6ffb37498c939e11aa8744f74c25444b
Nick Oliger <naoliger@edu.hse.ru>
2024-01-12 19:46:29 +0300

    12/01/24 | Add solution for task #32

---
+++ tasks/0032-remove-element.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int removeElement(vector<int> &nums, int val) {
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] == val) {
+                nums[i] = nums.back();
+                nums.pop_back();
+            } else {
+                i++;
+            }
+        }
+        return nums.size();
+    }
+};
