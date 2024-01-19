1fc4b9614b98ce0f47e0038f41f7a395f0330e38
Nick Oliger <naoliger@edu.hse.ru>
2024-01-19 03:10:39 +0300

    19/01/24 | Add great solution for task #55

---
+++ tasks/0055-merge-intervals.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        auto cmp = [](vector<int> &a, vector<int> &b) {
+            if (a[0] == b[0]) {
+                return a[1] < b[1];
+            }
+            return a[0] < b[0];
+        };
+        sort(intervals.begin(), intervals.end(), cmp);   // O(n*log(n)) runtime
+        int last = 0;                                    // O(1) memory
+        int j;
+        for (int i = 0; i < intervals.size();) {
+            j = i + 1;
+            while (j < intervals.size() && intervals[j][0] <= intervals[i][1]) {
+                intervals[i][1] = max(intervals[i][1], intervals[j][1]);
+                j++;
+            }
+            if (last != i) {
+                intervals[last] = intervals[i];
+            }
+            last++;
+            i = j;
+        }
+        vector<vector<int>> result(intervals.begin(), intervals.begin() + last);
+        return result;
+        // intervals.erase(intervals.begin() + last, intervals.end());
+        // return intervals;
+    }
+};
