1d09bc3a3b1f07b5920e3bee60f5641cf8541c2f
Nick Oliger <naoliger@edu.hse.ru>
2024-01-23 00:30:55 +0300

    23/01/24 | Update solution with an explanation for task #1

--- amazon/0001-optimal-partition-of-string.cpp
+++ amazon/0001-optimal-partition-of-string.cpp
@@ -24,3 +24,30 @@ class Solution {
         return result;
     }
 };
+
+// Correct solution
+// TODO: Verify my explanation of this (most optimal) solution:
+// v = 2^0 * x_0 + 2^1 * x_1 + ... + 2^25 * x_25
+// n = 26 letters in the alphabet
+// if we had already encountered some char CUR, we need to check
+// (CUR - 'a')th bit. If it is equal to 1, we set v = 0;
+// Otherwise, we increment (CUR - 'a')th bit (v += 2^(...))
+
+/*
+class Solution {
+public:
+    int partitionString(string s) {
+        int ans = 1;
+        int v = 0;
+        for (char c : s) {
+            int i = c - 'a';
+            if ((v >> i) & 1) {
+                v = 0;
+                ++ans;
+            }
+            v |= 1 << i;
+        }
+        return ans;
+    }
+};
+*/
