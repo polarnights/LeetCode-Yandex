02c609dd218c7a4cceef356ba1417045b4fbd77b
Nick Oliger <naoliger@edu.hse.ru>
2024-02-04 07:42:27 +0300

    04/02/24 | Add solution for task #99

---
+++ tasks/0099-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int singleNumber(vector<int> &nums) {
+        int result = 0;
+        for (auto &elem : nums) {
+            result ^= elem;
+        }
+        return result;
+    }
+};
